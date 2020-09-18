45363268f3dffe571929a105366b4255fec4fdfc
Manuela Uribe <manuelauribe@MacBook-Pro-de-Manuela.local>
2020-09-18 12:30:31 -0500

    Vista final de las imagenes

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,18 +1,20 @@
 <template>
-    <v-card>
-        <h1>Aquí van las imagenes</h1>
-        <button @click="imprimir">Imprimir</button>
-    </v-card>
+  <v-container>
+    <v-row>
+      <v-col v-bind:key="item.id" v-for="item in items.hits" sm="4" cols="12">
+        <v-card class="ma-5" max-width="600">
+          <img class="ma-2" :src="item.previewURL" alt />
+          <v-card-title>Propiedad de {{item.user}}</v-card-title>
+          <v-card-subtitle>Tags: {{item.tags}}</v-card-subtitle>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-    name : 'ImageList',
-    props: ["items"],
-    methods:{
-       imprimir(){
-           console.log(this.items)
-       }
-    }
-}
+  name: "ImageList",
+  props: ["items"],
+};
 </script>
